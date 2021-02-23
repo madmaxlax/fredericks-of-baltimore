33946d485a28cfe55ddffd8e2bc2a47bd80831c4
Max Struever <struever.max@bcgplatinion.com>
2021-02-23 00:43:52 -0500

    adding assets from grayscale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
