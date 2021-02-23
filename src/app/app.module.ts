dbfa235f556700b0c5a08997444af7d4bb33648c
Max Struever <struever.max@bcgplatinion.com>
2021-02-23 01:52:11 -0500

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductsComponent, ProductDetailComponent, HomeComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
