90884d9ee5cb7af547b75083e4baf0e9aca4006e
Max Struever <maxstruever@gmail.com>
2024-11-14 23:19:41 -0500

    working

---
+++ src/assets/grayscale/js/trello labels.js
@@ -0,0 +1,12 @@
+{
+"Stonework & Masonry":"6736c5d79d09c07b8ae43525",
+"Doors & Frames":"602d96ee86c6bc9cc56228df",
+"Hardware":"602d96ee86c6bc9cc56228d5",
+"Tools & Machinery":"6736c5e1cb686413af640777",
+"Decorative Elements":"6736c5c91689592d1fa11b5b",
+"Glass & Mirrors":"602d96ee86c6bc9cc56228da",
+"Woodwork":"6736c5cfc3a579e1ffade976",
+"Furniture & Seating":"602d96ee86c6bc9cc56228de",
+"Signs & Lettering":"602d96ee86c6bc9cc56228db",
+"Metalwork":"6736c5c0bf1c1ad986469313",
+"Lighting":"602d96ee86c6bc9cc56228d7"}
