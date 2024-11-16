74dc2fb86306829812b02ea703a295c67b8e353c
Max Struever <maxstruever@gmail.com>
2024-11-16 13:39:36 -0500

    redirect back to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'products', component: ProductsComponent },
-  // { path: 'product-detail/:id', component: ProductDetailComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: 'product-detail/:id', component: ProductDetailComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
